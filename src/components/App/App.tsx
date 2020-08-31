5195c47df779cba034984ff4687f68ce73c80646
Thibault Goehringer <tgoehringer@gmail.com>
2020-08-31 16:35:51 +0200

    Add message to App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,8 @@ import { h, FunctionComponent } from 'preact';
 
 import style from './App.css';
 
-const App: FunctionComponent = () => <div className={style.app} />;
+const App: FunctionComponent = () => (
+  <div className={style.app}>Hello World !</div>
+);
 
 export default App;
