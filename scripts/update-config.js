a2cd1a25c2f6f242f68943067bf138d24e98dce0
Thibault Goehringer <tgoehringer@gmail.com>
2020-09-05 15:01:19 +0200

    Ignore es-lint rules in the update-config script

--- scripts/update-config.js
+++ scripts/update-config.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
