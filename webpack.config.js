c526163d0c654404662a2df6c7960aa45491d824
Thibault Goehringer <tgoehringer@gmail.com>
2020-09-08 12:01:01 +0200

    Add typescript definitions for images import

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = (_, argv) => {
           ],
         },
         {
-          test: /\.(woff(2)?|ttf|eot|svg|png|jpg|svg)$/,
+          test: /\.(woff(2)?|(o|t)tf|eot|png|jpg|svg)$/,
           use: [
             {
               loader: 'file-loader',
