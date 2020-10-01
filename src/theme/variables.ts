0afabf754106d6b232023785806c3c37c3233a88
Thibault Goehringer <tgoehringer@gmail.com>
2020-10-01 13:15:54 +0200

    Remove unused variable.ts file

--- src/theme/variables.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  customProperties: {
-    '--color-main-1': '#0000FF',
-    '--color-main-2': '#FF0000',
-    '--color-black': '#000000',
-    '--color-white': '#FFFFFF',
-  },
-};
